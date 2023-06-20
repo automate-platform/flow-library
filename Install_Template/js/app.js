189f8e7f1f269b59d71d1a141fd3b10591f1619d
Vũ Đào Ngọc Hải <haiqd065@gmail.com>
2023-06-20 10:31:34 +0700

    Update Tab Detail, Tab Version, Tab Header

--- Install_Template/js/app.js
+++ Install_Template/js/app.js
@@ -1,14 +1,14 @@
 
-const templateList = document.querySelector('.monaco-list-rows').innerHTML;
-const templateDetail = document.querySelector('.split-view-view').innerHTML;
-const base_url = "http://127.0.0.1:1880";
-const typingTimer = '';
+let templateList = document.querySelector('.monaco-list-rows').innerHTML;
+let templateDetail = document.querySelector('.editor-container').innerHTML;
+let tabHeader = document.querySelector('.tabs-container').innerHTML;
+let base_url = "http://127.0.0.1:1880";
 let recommendApp = [];
 let installedApp = [];
 let currentApp = {};
 
-
 window.onload = function () {
+    document.querySelector('.tabs-container').innerHTML = '';
     fetch('http://127.0.0.1:1880/app')
         .then(response => response.json()).then(data => {
             document.querySelector('.monaco-list-rows').innerHTML = '';
@@ -23,24 +23,86 @@ window.onload = function () {
 }
 
 
-function openDetailTab(_id) {
+function openDetail(_id) {
+    let tem = document.querySelector('.monaco-scrollable-element').innerHTML
+    if (tem.trim() === "") {
+        var newDiv = document.createElement("div");
+        newDiv.className = "tabs-container";
+        var container = document.querySelector('.monaco-scrollable-element');
+        container.appendChild(newDiv);
+    }
     document.querySelector('.split-view-view').style.display = 'block';
     fetch('http://127.0.0.1:1880/app/' + _id)
         .then(response => response.json()).then(data => {
-            document.querySelector('.split-view-view').innerHTML = '';
-            // console.log(data)
-            const rendered = Mustache.render(templateDetail, data);
-            document.querySelector('.split-view-view').innerHTML = rendered;
+            document.querySelector('.editor-container').innerHTML = '';
+            const renderedData = Mustache.render(templateDetail, data);
+            const renderedTab = Mustache.render(tabHeader, data);
+            const allTabs = document.querySelectorAll('.tab-actions-right');
+            // allTabs.forEach((tab) => {
+            //     if (!tab.id === (_id + ' ').trim()) {
+            //         document.querySelector('.editor-container').innerHTML = renderedData;
+            //         document.querySelector('.tabs-container').innerHTML += renderedTab;
+            //         var tabs = document.querySelectorAll('.tab-actions-right')
+            //         tabs.forEach((tab) => tab.classList.remove('active'));
+            //         tabs[tabs.length - 1].classList.add('active')
+            //     }
+            // });
+            document.querySelector('.editor-container').innerHTML = renderedData;
+            document.querySelector('.tabs-container').innerHTML += renderedTab;
+            var tabs = document.querySelectorAll('.tab-actions-right')
+            tabs.forEach((tab) => tab.classList.remove('active'));
+            tabs[tabs.length - 1].classList.add('active')
+
         })
         .catch(function (error) {
             console.error(error);
         });
 }
 
+
+function openDetailTab(_id) {
+    document.querySelector('.split-view-view').style.display = 'block';
+    fetch('http://127.0.0.1:1880/app/' + _id)
+        .then(response => response.json()).then(data => {
+            document.querySelector('.editor-container').innerHTML = '';
+            const renderedData = Mustache.render(templateDetail, data);
+            document.querySelector('.editor-container').innerHTML = renderedData;
+
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+    let tabClose = document.querySelectorAll(".tab-actions-right");
+    tabClose.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabClose.forEach((tabActive) => tabActive.classList.remove('active'));
+            tab.classList.add('active')
+        });
+    });
+}
+
+function closeTab() {
+    let closeBtns = document.querySelectorAll(".tab-actions");
+    let tabClose = document.querySelectorAll(".tab-actions-right");
+    const count = tabClose.length
+    closeBtns.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabClose[index].remove();
+            count--;
+        });
+    });
+    if (count <= 1) {
+        document.querySelector('.editor-instance').remove()
+        document.querySelector('.tabs-container').remove()
+    }
+    tabClose.forEach((tabActive) => tabActive.classList.remove('active'));
+    tabClose[0].classList.add('active')
+}
+
 function showSpinner(isShow) {
-    if (isShow === true) {
+    if (isShow) {
         document.querySelector('.spinner-custom.success').style.display = 'block';
-        document.querySelector('.background.').style.display = 'block';
+        document.querySelector('.background').style.display = 'block';
         setTimeout(() => {
             document.querySelector('.spinner-custom.success').style.display = 'none';
             document.querySelector('.background').style.display = 'none';
@@ -88,8 +150,8 @@ function installExtension(_id) {
                     // }
                     showSpinner(true);
                 })
-                .catch(error => {
-                    console.log('error', error);
+                .catch(function (error) {
+                    console.error(error);
                     showSpinner(false);
                 });
         })
@@ -98,30 +160,30 @@ function installExtension(_id) {
         });
 }
 
-function getResource(appId) {
-    var header = new Headers();
-    header.append("Accept", "application/json");
-    header.append("Node-RED-API-Version", "v2");
+// function getResource(appId) {
+//     var header = new Headers();
+//     header.append("Accept", "application/json");
+//     header.append("Node-RED-API-Version", "v2");
 
-    var requestOptions = {
-        method: "GET",
-        headers: header,
-        redirect: "follow",
-    };
+//     var requestOptions = {
+//         method: "GET",
+//         headers: header,
+//         redirect: "follow",
+//     };
 
-    fetch(`${base_url}/app/${appId}/install`, requestOptions)
-        .then((response) => response.text())
-        .then((result) => {
-            showMessage("install extension successfully");
-            showSpinner(false);
-            console.log(result);
-        })
-        .catch((error) => { showSpinner(false); console.log("error", error) });
-}
+//     fetch(`${base_url}/app/${appId}/install`, requestOptions)
+//         .then((response) => response.text())
+//         .then((result) => {
+//             showMessage("install extension successfully");
+//             showSpinner(false);
+//             console.log(result);
+//         })
+//         .catch((error) => { showSpinner(false); console.log("error", error) });
+// }
 
 
 $(document).ready(function () {
-    $('#myForm').on('input', function(event) {
+    $('#myForm').on('input', function (event) {
         event.preventDefault();
         const desc = document.querySelector('#searchForm').value
         $.ajax({
--- Install_Template/js/main.js
+++ Install_Template/js/main.js
@@ -4,8 +4,29 @@ $('.pane-header').on('click', function () {
   $(".monaco-list-rows").slideToggle("fast");
   if (clickCount % 2 === 1) {
     $('.bx-chevron-right').removeClass("bx-rotate-90");
-  }else{
+  } else {
     $('.bx-chevron-right').addClass("bx-rotate-90");
   }
 });
 
+let tabs = document.querySelectorAll(".tab-header");
+
+// tabs.forEach((tab, index) => {
+//     tab.addEventListener('click', () =>{
+
+
+//         contents.forEach((c) => c.classList.remove("active"));
+//         contents[index].classList.add("active");
+//     });
+// });
+
+tabs[0].click();
+
+function ClickFunc(tab, content) {
+  let tabs = document.querySelectorAll(".tab-header");
+  let contents = document.querySelectorAll(".tab-content");
+  tabs.forEach((tabb) => tabb.classList.remove("active"));
+  tab.addClass("active");
+  contents.forEach((c) => c.classList.remove("active"));
+  content.addClass("active");  
+}
