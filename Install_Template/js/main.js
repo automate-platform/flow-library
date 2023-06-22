9b998ba31923bd0272c745655b5c406465e90613
Vũ Đào Ngọc Hải <haiqd065@gmail.com>
2023-06-22 15:42:53 +0700

    Install App, Delete App

--- Install_Template/js/main.js
+++ Install_Template/js/main.js
@@ -1,11 +1,24 @@
-var clickCount = 0;
-$('.pane-header').on('click', function () {
-  clickCount++;
-  $(".monaco-list-rows").slideToggle("fast");
-  if (clickCount % 2 === 1) {
-    $('.bx-chevron-right').removeClass("bx-rotate-90");
+let clickCountNot = 0;
+$('.app-not-installed .pane-header').on('click', function () {
+  clickCountNot++;
+  if (clickCountNot % 2 === 1) {
+    $(".app-not-installed .monaco-list").animate({height:'0px'},300);
+    $('.app-not-installed .bx-chevron-right').removeClass("bx-rotate-90");
   } else {
-    $('.bx-chevron-right').addClass("bx-rotate-90");
+    $('.app-not-installed .bx-chevron-right').addClass("bx-rotate-90");
+    $(".app-not-installed .monaco-list").animate({height:'300px'},300);
+  }
+});
+
+var clickCountYes = 0;
+$('.app-installed .pane-header').on('click', function () {
+  clickCountYes++;
+  if (clickCountYes % 2 === 1) {
+    $(".app-installed .monaco-list").animate({height:'0px'},300);
+    $('.app-installed .bx-chevron-right').removeClass("bx-rotate-90");
+  } else {
+    $('.app-installed .bx-chevron-right').addClass("bx-rotate-90");
+    $(".app-installed .monaco-list").animate({height:'300px'},300);
   }
 });
 
@@ -28,5 +41,44 @@ function ClickFunc(tab, content) {
   tabs.forEach((tabb) => tabb.classList.remove("active"));
   tab.addClass("active");
   contents.forEach((c) => c.classList.remove("active"));
-  content.addClass("active");  
+  content.addClass("active");
 }
+
+// // Get the resizable div and resize handle elements
+// const resizableDiv = document.getElementById('resizableDiv');
+// const resizeHandle = document.getElementById('resizeHandle');
+
+// let isResizing = false;
+// let startY, startHeight, originalHeight;
+// const minHeight = 260; // Minimum height for the resizable div
+// const maxHeight = 900; // Minimum height for the resizable div
+
+
+// // Function to handle the resizing
+// function resizeDiv(event) {
+//   if (!isResizing) return;
+
+//   let deltaY = startY - event.clientY;
+//   let newHeight = startHeight + deltaY;
+
+//   if (newHeight >= minHeight && newHeight <= maxHeight) {
+//     resizableDiv.style.height = newHeight + 'px';
+//     resizableDiv.style.top = originalHeight - newHeight + 'px';
+//     console.log(resizableDiv.style.height);
+//   }
+
+// }
+
+// // Event listener for starting the resizing
+// resizeHandle.addEventListener('mousedown', function (event) {
+//   isResizing = true;
+//   startY = event.clientY;
+//   startHeight = resizableDiv.offsetHeight;
+//   originalHeight = resizableDiv.offsetHeight + resizableDiv.offsetTop;
+// });
+
+// // Event listeners for resizing
+// document.addEventListener('mousemove', resizeDiv);
+// document.addEventListener('mouseup', function () {
+//   isResizing = false;
+// });
