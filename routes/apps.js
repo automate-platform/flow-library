bee3717efa0a929141a6e07954ce63e870007173
Vũ Đào Ngọc Hải <haiqd065@gmail.com>
2023-08-03 16:33:56 +0700

    Input node-module

--- routes/apps.js
+++ routes/apps.js
@@ -38,7 +38,7 @@ if (setting.template.apps) {
                     }
                 }
             };
-            apper.create(req.session.accessToken, app_post, req.body.tags || []).then(function (id) {
+            apper.create(req.session.accessToken, app_post, req.body.tags || [], req.body.modules || []).then(function (id) {
                 let url = '/add/app/source?id=' + id + '&name=' + encodeURIComponent(req.body.title);
                 res.send(url);
             }).catch(function (err) {
@@ -71,7 +71,7 @@ if (setting.template.apps) {
                 let files = req.files;
                 files.forEach(file => {
                     if (file.mimetype === 'application/zip' ||
-                        file.mimetype === 'application/x-zip-compressed' ||  path.extname(file.originalname) === '.zip') {
+                        file.mimetype === 'application/x-zip-compressed' || path.extname(file.originalname) === '.zip') {
                         zipFileName = file.originalname;
                     }
                     if (file.mimetype.startsWith('image/')) {
@@ -292,7 +292,7 @@ if (setting.template.apps) {
                 }
             }
         };
-        apper.update(req.body.id, app_post, req.body.tags || [])
+        apper.update(req.body.id, app_post, req.body.tags || [], req.body.modules || [])
             .then(function (data) {
                 let url = `/update/app/source/${req.body.id}`;
 
@@ -463,6 +463,5 @@ if (setting.template.apps) {
         context.display = setting.template;
         res.send(mustache.render(templates.addSourceApp, context, templates.partials));
     });
-
     module.exports = app;
 }
